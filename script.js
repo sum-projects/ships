97f902db19b27d7881c85662d78de27e379a5ee9
sUm <jakub.sumara09@gmail.com>
2025-04-27 10:40:25 +0200

    Players name

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+// Screens
 const setupScreen = document.querySelector('#setup-screen');
 const placementScreen = document.querySelector('#placement-screen');
 const gameScreen = document.querySelector('#game-screen');
 
+// Buttons
 const startSetupBtn = document.querySelector('#start-setup-btn');
 const confirmPlacementBtn = document.querySelector('#confirm-placement-btn');
 
+// Inputs
+const player1NameInput = document.querySelector('#player1-name');
+const player2NameInput = document.querySelector('#player2-name');
+
+// State
+const state = {
+    players: [
+        {
+            name: 'Gracz 1'
+        },
+        {
+            name: 'Gracz 2'
+        }
+    ]
+};
+
 startSetupBtn.addEventListener('click', () => {
+    state.players[0].name = player1NameInput.value;
+    state.players[1].name = player2NameInput.value;
+
     setupScreen.classList.add('hidden');
     placementScreen.classList.remove('hidden');
 });
