d610d00619740cca0283115a3e43cda82ded6e71
sUm <jakub.sumara09@gmail.com>
2025-04-27 10:20:01 +0200

    change screen game

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const setupScreen = document.querySelector('#setup-screen');
 const placementScreen = document.querySelector('#placement-screen');
+const gameScreen = document.querySelector('#game-screen');
 
 const startSetupBtn = document.querySelector('#start-setup-btn');
+const confirmPlacementBtn = document.querySelector('#confirm-placement-btn');
 
 startSetupBtn.addEventListener('click', () => {
     setupScreen.classList.add('hidden');
     placementScreen.classList.remove('hidden');
+});
+
+confirmPlacementBtn.addEventListener('click', () => {
+    placementScreen.classList.add('hidden');
+    gameScreen.classList.remove('hidden');
 });
