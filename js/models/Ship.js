2aae350bb73891dd1c8528837678525046dbc91b
sUm <jakub.sumara09@gmail.com>
2025-05-07 16:38:32 +0200

    Ship object

--- js/models/Ship.js
+++ js/models/Ship.js
@@ -1,36 +1,58 @@
 ﻿class Ship {
-    isValidPlacement(x, y, shipSize, isHorizontal) {
-        if (isHorizontal) {
-            if (x + shipSize > CONSTANTS.BOARD_SIZE) return false;
-        } else {
-            if (y + shipSize > CONSTANTS.BOARD_SIZE) return false;
+    constructor(size, direction, x, y) {
+        this.size = size;
+        this.direction = direction;
+        this.x = x;
+        this.y = y;
+        this.cells = this.calculateCells();
+    }
+
+    calculateCells() {
+        const cells = [];
+        const isHorizontal = this.direction === CONSTANTS.DIRECTION_HORIZONTAL;
+
+        for (let i = 0; i < this.size; i++) {
+            const cellX = isHorizontal ? this.x + i : this.x;
+            const cellY = isHorizontal ? this.y : this.y + i;
+            cells.push([cellX, cellY]);
         }
 
-        // Sprawdź, czy statek nie koliduje z innymi statkami
-        for (let i = 0; i < state.placement.shipSize; i++) {
-            const cellX = isHorizontal ? x + i : x;
-            const cellY = isHorizontal ? y : y + i;
+        return cells;
+    }
 
-            // Sprawdzenie, czy pole jest już zajęte
-            if (state.players[state.currentPlayer].boards[cellY][cellX] === 1) {
-                return false;
+    isValid() {
+        const isHorizontal = this.direction === CONSTANTS.DIRECTION_HORIZONTAL;
+
+        if (isHorizontal) {
+            if (this.x + this.size > CONSTANTS.BOARD_SIZE) return false;
+        } else {
+            if (this.y + this.size > CONSTANTS.BOARD_SIZE) return false;
+        }
+
+
+        return true;
+    }
+
+    hasCollision(board) {
+        for (const [x, y] of this.cells) {
+            if (board[y][x] === CONSTANTS.CELL_SHIP) {
+                return true;
             }
 
-            // Sprawdzenie sąsiednich pól (statki nie mogą się stykać)
             for (let dx = -1; dx <= 1; dx++) {
                 for (let dy = -1; dy <= 1; dy++) {
-                    const nx = cellX + dx;
-                    const ny = cellY + dy;
+                    const nx = x + dx;
+                    const ny = y + dy;
 
                     if (nx >= 0 && nx < CONSTANTS.BOARD_SIZE && ny >= 0 && ny < CONSTANTS.BOARD_SIZE) {
-                        if (state.players[state.currentPlayer].boards[ny][nx] === 1) {
-                            return false;
+                        if (board[ny][nx] === CONSTANTS.CELL_SHIP) {
+                            return true;
                         }
                     }
                 }
             }
         }
 
-        return true;
+        return false;
     }
 }
