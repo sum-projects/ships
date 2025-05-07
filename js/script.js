5217d51713412fbc3ffad6be332aa5ec97f2f7b4
sUm <jakub.sumara09@gmail.com>
2025-05-07 16:48:12 +0200

    UIController

--- js/script.js
+++ js/script.js
@@ -1,39 +1,3 @@
-const elements = {
-    // Screens
-    setupScreen: document.querySelector('#setup-screen'),
-    placementScreen: document.querySelector('#placement-screen'),
-    gameScreen: document.querySelector('#game-screen'),
-    gameOverScreen: document.querySelector('#game-over-screen'),
-
-    // Buttons
-    startSetupBtn: document.querySelector('#start-setup-btn'),
-    confirmPlacementBtn: document.querySelector('#confirm-placement-btn'),
-    rotateBtn: document.querySelector('#rotate-btn'),
-    restartBtn: document.querySelector('#restart-btn'),
-
-    // Inputs
-    player1NameInput: document.querySelector('#player1-name'),
-    player2NameInput: document.querySelector('#player2-name'),
-
-    // Boards
-    placementBoard: document.querySelector('#placement-board'),
-    player1Board: document.querySelector('#player1-board'),
-    player2Board: document.querySelector('#player2-board'),
-
-    // Game Elements
-    placementInfo: document.querySelector('#placement-info'),
-    shipCounters: {
-        2: document.querySelector('#ship-2'),
-        3: document.querySelector('#ship-3'),
-        4: document.querySelector('#ship-4')
-    },
-    currentPlayerSpan: document.querySelector('#current-player'),
-    player1Header: document.querySelector('#player1-header'),
-    player2Header: document.querySelector('#player2-header'),
-    player1Ships: document.querySelector('#player1-ships'),
-    player2Ships: document.querySelector('#player2-ships'),
-    winnerName: document.querySelector('#winner-name'),
-};
 
 const state = {
     placement: {
@@ -46,7 +10,6 @@ const state = {
         },
     },
     currentPlayer: 0,
-    currentScreen: CONSTANTS.SCREEN.SETUP,
 };
 
 
